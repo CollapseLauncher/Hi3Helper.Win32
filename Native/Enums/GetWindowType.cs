6edb1e376046c4f3ad8cd661e6d8585742b2f8fb
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-11-17 20:16:58 +0700

    Update GetWindowType.cs

--- Native/Enums/GetWindowType.cs
+++ Native/Enums/GetWindowType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hi3Helper.Win32.Native.Enums
+﻿namespace Hi3Helper.Win32.Native.Enums
 {
     public enum GetWindowType : uint
     {
