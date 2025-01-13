b62180e69319c29b5dba34c8e57f8692087c6429
shatyuka <shatyuka@qq.com>
2025-01-13 15:08:23 +0800

    Try catch TaskbarList

--- TaskbarListCOM/TaskbarList.cs
+++ TaskbarListCOM/TaskbarList.cs
@@ -6,30 +6,54 @@ namespace Hi3Helper.Win32.TaskbarListCOM
 {
     public class TaskbarList
     {
-        private ITaskbarList3? m_taskbarList;
+        private readonly ITaskbarList3? m_taskbarList;
 
         public TaskbarList()
         {
-            ComMarshal.CreateInstance(new Guid(CLSIDGuid.CLSID_TaskbarList),
-                                      0,
-                                      CLSCTX.CLSCTX_INPROC_SERVER,
-                                      out ITaskbarList3? taskbarList).ThrowOnFailure();
-            m_taskbarList = taskbarList;
+            try
+            {
+                ComMarshal.CreateInstance(new Guid(CLSIDGuid.CLSID_TaskbarList),
+                                          0,
+                                          CLSCTX.CLSCTX_INPROC_SERVER,
+                                          out ITaskbarList3? taskbarList).ThrowOnFailure();
+                m_taskbarList = taskbarList;
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
         }
 
         ~TaskbarList()
         {
-            ComMarshal.FreeInstance(m_taskbarList);
+            if (m_taskbarList != null)
+            {
+                ComMarshal.FreeInstance(m_taskbarList);
+            }
         }
 
         public int SetProgressState(nint hwnd, TaskbarState state)
         {
-            return m_taskbarList!.SetProgressState(hwnd, state);
+            try
+            {
+                return m_taskbarList?.SetProgressState(hwnd, state) ?? 0;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
         }
 
         public int SetProgressValue(nint hwnd, ulong ullCompleted, ulong ullTotal)
         {
-            return m_taskbarList!.SetProgressValue(hwnd, ullCompleted, ullTotal);
+            try
+            {
+                return m_taskbarList?.SetProgressValue(hwnd, ullCompleted, ullTotal) ?? 0;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 }
