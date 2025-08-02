097425444ab22f5ac7a24af6107fce5fcc9b2e44
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-08-02 07:52:09 +0700

    Adding new async methods for DnsQuery
    This utilizes DnsQueryEx, which is capable for performing asynchronous DNS query operation natively, instead of artificial async (by using Task.StartNew/Task.Run).

--- Native/LibraryImport/PInvoke.DnsApi.cs
+++ Native/LibraryImport/PInvoke.DnsApi.cs
@@ -15,6 +15,14 @@ namespace Hi3Helper.Win32.Native.LibraryImport
         [LibraryImport("dnsapi.dll")]
         public static unsafe partial void DnsRecordListFree(DNS_RECORD* ppQueryResultsSet, DNS_FREE_TYPE freeType);
 
+        [LibraryImport("dnsapi.dll", EntryPoint = "DnsQueryEx", SetLastError = true)]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        public static partial DnsStatus DnsQueryEx(in DNS_QUERY_REQUEST3 requestQuery, ref DNS_QUERY_RESULT queryResult, ref DNS_QUERY_CANCEL queryCancel);
+
+        [LibraryImport("dnsapi.dll", EntryPoint = "DnsCancelQuery", SetLastError = true)]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        public static partial DnsStatus DnsCancelQuery(in DNS_QUERY_CANCEL queryCancel);
+
         public static unsafe void DnsRecordListFree(nint ppQueryResultsSet, DNS_FREE_TYPE freeType)
             => DnsRecordListFree((DNS_RECORD*)ppQueryResultsSet, freeType);
     }
--- Native/Structs/Dns/DNS_RECORD.cs
+++ Native/Structs/Dns/DNS_RECORD.cs
@@ -11,16 +11,16 @@ namespace Hi3Helper.Win32.Native.Structs.Dns
     /// See http://msdn.microsoft.com/en-us/library/windows/desktop/ms682082(v=vs.85).aspx
     /// These field offsets could be different depending on endianness and bitness
     /// </summary>
-    public readonly unsafe ref struct DNS_RECORD
+    public readonly unsafe struct DNS_RECORD
     {
-        public readonly DNS_RECORD* pNext;
-        public readonly char* pName;
-        public readonly ushort wType;
-        public readonly ushort wDataLength;
+        public readonly DNS_RECORD*   pNext;
+        public readonly char*         pName;
+        public readonly ushort        wType;
+        public readonly ushort        wDataLength;
         public readonly DnsFlagsUnion Flags;
-        public readonly uint dwTtl;
-        public readonly uint dwReserved;
-        public readonly DnsDataUnion Data;
+        public readonly uint          dwTtl;
+        public readonly uint          dwReserved;
+        public readonly DnsDataUnion  Data;
 
         public ReadOnlySpan<char> GetRecordName() => MemoryMarshal.CreateReadOnlySpanFromNullTerminated(pName);
 
