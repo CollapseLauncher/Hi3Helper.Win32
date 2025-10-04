4d27612f354f8c608bb6fc8704274321970dc00a
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-10-04 17:04:32 +0700

    Add TryCreateMiniDump to accept SafeHandle

--- Native/LibraryImport/PInvoke.DbgHelp.cs
+++ Native/LibraryImport/PInvoke.DbgHelp.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Runtime.InteropServices;
+using Hi3Helper.Win32.Native.Enums;
 
 namespace Hi3Helper.Win32.Native.LibraryImport
 {
     public static partial class PInvoke
     {
-        [LibraryImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", StringMarshalling = StringMarshalling.Utf16)]
+        [LibraryImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static partial bool MiniDumpWriteDump(
-            IntPtr     hProcess,
-            int        processId,
-            SafeHandle hFile,
-            int        dumpType,
-            IntPtr     exceptionParam,
-            IntPtr     userStreamParam,
-            IntPtr     callbackParam);
+            IntPtr       hProcess,
+            int          processId,
+            nint         hFile,
+            MiniDumpType dumpType,
+            IntPtr       exceptionParam,
+            IntPtr       userStreamParam,
+            IntPtr       callbackParam);
     }
 }
