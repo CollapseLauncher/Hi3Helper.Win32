0ce5a592a2a8980d2fe35aa6b8a01bd74107e1ae
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-01-17 23:31:40 +0700

    Add helper to get SwapChainPanelHelper VTables + Other improvement
    This reduces the amount of emitted JIT-generated assembly while invoking direct COM ABI function. Instead of allocating it to a delegate in each of the calls, we cache them.
    
    Also, switch to use ICanvasBitmap caller for DrawImage as it has the most shortest call routines within Win2D APIs, which also saves CPU cycles.
    https://github.com/microsoft/Win2D/blob/65e90b29055de64b02e7f2a3d3f042b7fa36326c/winrt/lib/drawing/CanvasDrawingSession.cpp#L254
    
    Anywho, this change require RuntimeMarshalling to be enabled

--- WinRT/SwapChainPanelHelper/SwapChainPanelHelper.cs
+++ WinRT/SwapChainPanelHelper/SwapChainPanelHelper.cs
@@ -1,7 +1,7 @@
-﻿using Hi3Helper.Win32.Native.Structs;
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Windows.Foundation;
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable IdentifierTypo
@@ -15,39 +15,86 @@ public static class SwapChainPanelHelper
     /// BEWARE: This IID is different from regular IDisposable.
     /// </summary>
     private static readonly Guid IDisposableWinRTObj_IID = new("30d5a829-7fa4-4026-83bb-d75bae4ea99e");
+    private const uint DefaultDummyColor = 0xCEFABEBA; // Just a random
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void NativeSurfaceImageSource_BeginDrawEndDrawUnsafe(
-        nint    imageSourceP,
-        nint    renderTargetP,
-        in Rect updateRect)
+    [SkipLocalsInit]
+    public static unsafe void GetDirectNativeDelegateForDrawRoutine(
+        nint                                                                 imageSourceP,
+        nint                                                                 renderTargetP,
+        out delegate* unmanaged[Stdcall]<nint, uint, in Rect, out nint, int> s_beginDraw,
+        out delegate* unmanaged[Stdcall]<nint, nint, in Rect, int>           s_drawImage,
+        out delegate* unmanaged[Stdcall]<nint, int>                          s_dispose,
+        in  Rect                                                             updateWinRect)
     {
-        nint drawingSessionPpv = nint.Zero;
-        uint colorDummy        = 0;
+        // -- CanvasImageSource.CreateDrawingSession(Color, Rect);
+        s_beginDraw = (delegate* unmanaged[Stdcall]<nint, uint, in Rect, out nint, int>)(*(*(void***)imageSourceP + 7));
+        s_beginDraw(imageSourceP, DefaultDummyColor, in updateWinRect, out nint drawingSessionPpv);
 
-        void* colorDummyP = Unsafe.AsPointer(in colorDummy);
-        void* updateRectP = Unsafe.AsPointer(in updateRect);
-
-        // -- CanvasImageSource.CreateDrawingSession(Color);
-        Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<nint, void*, void**, int>)(*(*(void***)imageSourceP + 6)))(imageSourceP, colorDummyP, (void**)&drawingSessionPpv));
-
-        // -- CanvasDrawingSession.DrawImage(ICanvasImage);
-        Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<nint, void*, void*, int>)(*(*(void***)drawingSessionPpv + 9)))(drawingSessionPpv, (void*)renderTargetP, updateRectP));
+        // -- CanvasDrawingSession.DrawImage(ICanvasBitmap, Rect);
+        //    This method is the shortest based on the implementation source at:
+        //    https://github.com/microsoft/Win2D/blob/65e90b29055de64b02e7f2a3d3f042b7fa36326c/winrt/lib/drawing/CanvasDrawingSession.cpp#L254
+        s_drawImage = (delegate* unmanaged[Stdcall]<nint, nint, in Rect, int>)(*(*(void***)drawingSessionPpv + 12));
+        s_drawImage(drawingSessionPpv, renderTargetP, in updateWinRect);
 
         // -- Query to WinRT's IDisposable
-        Marshal.QueryInterface(drawingSessionPpv, in IDisposableWinRTObj_IID, out nint disposablePpv);
-        
+        QueryInterfaceShort(drawingSessionPpv, in IDisposableWinRTObj_IID, out nint disposablePpv);
+
         // -- CanvasDrawingSession.Dispose()
-        Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<nint, int>)(*(*(void***)disposablePpv + 6)))(disposablePpv));
+        s_dispose = (delegate* unmanaged[Stdcall]<nint, int>)(*(*(void***)disposablePpv + 6));
+        s_dispose(disposablePpv);
 
         // -- Release object
-        Marshal.Release(drawingSessionPpv);
-        Marshal.Release(disposablePpv);
+        ReleaseShort(drawingSessionPpv);
+        ReleaseShort(disposablePpv);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [SkipLocalsInit]
+    public static unsafe void NativeSurfaceImageSource_BeginDrawEndDrawUnsafe(
+        nint                                                             imageSourceP,
+        nint                                                             renderTargetP,
+        delegate* unmanaged[Stdcall]<nint, uint, in Rect, out nint, int> s_beginDraw,
+        delegate* unmanaged[Stdcall]<nint, nint, in Rect, int>           s_drawImage,
+        delegate* unmanaged[Stdcall]<nint, int>                          s_dispose,
+        in Rect                                                          updateWinRect)
+    {
+        // -- CanvasImageSource.CreateDrawingSession(Color, Rect);
+        Marshal.ThrowExceptionForHR(s_beginDraw(imageSourceP, DefaultDummyColor, in updateWinRect, out nint drawingSessionPpv));
+
+        // -- CanvasDrawingSession.DrawImage(ICanvasBitmap, Rect);
+        //    This method is the shortest based on the implementation source at:
+        //    https://github.com/microsoft/Win2D/blob/65e90b29055de64b02e7f2a3d3f042b7fa36326c/winrt/lib/drawing/CanvasDrawingSession.cpp#L254
+        Marshal.ThrowExceptionForHR(s_drawImage(drawingSessionPpv, renderTargetP, in updateWinRect));
+
+        // -- Query to WinRT's IDisposable
+        QueryInterfaceShort(drawingSessionPpv, in IDisposableWinRTObj_IID, out nint disposablePpv);
+
+        // -- CanvasDrawingSession.Dispose()
+        Marshal.ThrowExceptionForHR(s_dispose(disposablePpv));
+
+        // -- Release object
+        ReleaseShort(drawingSessionPpv);
+        ReleaseShort(disposablePpv);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [SkipLocalsInit]
     public static unsafe void MediaPlayer_CopyFrameToVideoSurfaceUnsafe(
-        nint playerP,
-        nint surfaceP)
-        => Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<nint, nint, int>)(*(*(void***)playerP + 10)))(playerP, surfaceP)); // +10 == .CopyFrameToVideoSurface(nint)
+        nint    playerP,
+        nint    surfaceP,
+        in Rect updateWinRect)
+        => Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<nint, nint, in Rect, int>)(*(*(void***)playerP + 11)))(playerP, surfaceP, in updateWinRect)); // +11 == .CopyFrameToVideoSurface(nint, Rect)
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [SkipLocalsInit]
+    public static unsafe int QueryInterfaceShort(nint pUnk, in Guid iid, out nint ppv)
+        => ((delegate* unmanaged<nint, in Guid, out nint, int>)
+            (**(void***)pUnk))(pUnk, in iid, out ppv);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [SkipLocalsInit]
+    public static unsafe int ReleaseShort(nint pUnk)
+        => ((delegate* unmanaged<nint, int>)
+            (*(*(void***)pUnk + 2)))(pUnk);
 }
